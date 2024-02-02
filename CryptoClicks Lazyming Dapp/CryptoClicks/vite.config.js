c4968ecdc08e46bc6e8cee4d89b8c651c7bce1d6
aheed <aheedkhan03@gmail.com>
2024-02-03 04:19:25 +0500

    web3 integration

--- CryptoClicks Lazyming Dapp/CryptoClicks/vite.config.js
+++ CryptoClicks Lazyming Dapp/CryptoClicks/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["firebase/app"],
-    },
-  },
+  plugins: [react()],
 });
